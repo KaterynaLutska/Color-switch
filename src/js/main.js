5e2f7e7b76233a268f8ecbf2b102fd9df891121b
Kateryna Lutska <ket.lutska@gmail.com>
2020-12-16 21:22:32 +0200

    hw 11

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+const refs = {
+  body: document.querySelector("body"),
+  buttonStart: document.querySelector(`[data-action="start"]`),
+  buttonStop: document.querySelector(`[data-action="stop"]`),
+};
+
+refs.buttonStart.addEventListener("click", bodyColor);
+refs.buttonStop.addEventListener("click", bodyColorStop);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}; // генерує випадкове число
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+let intervalId = null;
+let btnStartIsActive = false;
+
+function bodyColor() {
+  if (btnStartIsActive) {
+    return;
+  }
+  btnStartIsActive = true;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[`${randomIntegerFromInterval(0, colors.length - 1)}`];
+  }, 1000);
+  console.log("start");
+}
+
+function bodyColorStop() {
+  btnStartIsActive = false;
+  clearInterval(intervalId);
+  console.log("stop");
+}
